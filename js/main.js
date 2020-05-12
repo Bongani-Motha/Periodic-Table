9cca2855d4f37347c529e0684c08a11d0db254b4
Bongani Motha <motha.bongz@gmail.com>
2020-05-12 23:14:57 +0200

    add hover effect

--- js/main.js
+++ js/main.js
@@ -15,6 +15,9 @@ var react = document.getElementById('react'),
     ;
 
 // LANGUAGES
+
+
+
 var python = document.getElementById('python'),
     elixir = document.getElementById('elixir'),
     bash = document.getElementById('bash'),
@@ -56,8 +59,10 @@ var sqLite = document.getElementById('sqLite'),
     ;
 var frameWorks = document.getElementsByClassName('frameWorks');
 
-
-function frameworks(){
+/**
+ * WHEN YOU HOVER OVER FRAMEWORKS BUTTON, STYLE FRAMEWORKS AND DIM OTHER TECHNOLOGIES
+ */
+function frameworksHover(){
     react.style.transition = '0.2s';
     react.style.borderRadius = '10px';
    
@@ -94,7 +99,45 @@ function frameworks(){
     django.style.transition = '0.2s';
     django.style.borderRadius = '10px';
    
-   
+    (function () {
+        python.style.opacity = 0.4;
+        elixir.style.opacity = 0.4;
+        bash.style.opacity = 0.4;
+        ruby.style.opacity = 0.4;
+        typeScript.style.opacity = 0.4;
+        go.style.opacity = 0.4;
+        scala.style.opacity = 0.4;
+        erlang.style.opacity = 0.4;
+        kotlin.style.opacity = 0.4;
+        c_sharp.style.opacity = 0.4;
+        java.style.opacity = 0.4;
+        php.style.opacity = 0.4;
+        webAssembly.style.opacity = 0.4;
+        javaScript.style.opacity = 0.4;
+        c_plus.style.opacity = 0.4;
+        c.style.opacity = 0.4;
+        swift.style.opacity = 0.4;
+        dart.style.opacity = 0.4;
+        r.style.opacity = 0.4;
+        assembly.style.opacity = 0.4;
+        rust.style.opacity = 0.4;
+        clojure.style.opacity = 0.4;
+        sql.style.opacity = 0.4;
+        htmlCss.style.opacity = 0.4;
+
+        // DB Opacity
+        elastic.style.opacity = 0.4;
+        postgre.style.opacity = 0.4;
+        maria.style.opacity = 0.4;
+        sqLite.style.opacity = 0.4;
+        dynamo.style.opacity = 0.4;
+        redis.style.opacity = 0.4;
+        firebase.style.opacity = 0.4;
+        mongo.style.opacity = 0.4;
+        msSQL.style.opacity = 0.4;
+        node.style.opacity = 0.4;
+        mySQL.style.opacity = 0.4;
+    }());
 }
 
 function frameworksRevert() {
@@ -135,5 +178,446 @@ function frameworksRevert() {
     django.style.borderRadius = '0';
     django.style.fontWeight = 'normal';
 
+    (function() {
+        python.style.opacity = 1
+        python.style.opacity = 1
+        elixir.style.opacity = 1
+        bash.style.opacity = 1
+        ruby.style.opacity = 1
+        typeScript.style.opacity = 1
+        go.style.opacity = 1
+        scala.style.opacity = 1
+        erlang.style.opacity = 1
+        kotlin.style.opacity = 1
+        c_sharp.style.opacity = 1
+        java.style.opacity = 1
+        php.style.opacity = 1
+        webAssembly.style.opacity = 1
+        javaScript.style.opacity = 1
+        c_plus.style.opacity = 1
+        c.style.opacity = 1
+        swift.style.opacity = 1
+        dart.style.opacity = 1
+        r.style.opacity = 1
+        assembly.style.opacity = 1
+        rust.style.opacity = 1
+        clojure.style.opacity = 1
+        sql.style.opacity = 1
+        htmlCss.style.opacity = 1
+
+        // DB
+        elastic.style.opacity = 1;
+        postgre.style.opacity = 1;
+        maria.style.opacity = 1;
+        sqLite.style.opacity = 1;
+        dynamo.style.opacity = 1;
+        redis.style.opacity = 1;
+        firebase.style.opacity = 1;
+        mongo.style.opacity = 1;
+        msSQL.style.opacity = 1;
+        node.style.opacity = 1;
+        mySQL.style.opacity = 1;
+    }());
+}
+
+
+document.getElementById('frameWork-btn').addEventListener("mouseenter", frameworksHover);
+document.getElementById('frameWork-btn').addEventListener("mouseleave", frameworksRevert);
+
+/**
+ * WHEN YOU HOVER OVER LANGUAGES BUTTON STYLE LANGUAGES AND DIM OTHER TECHNOLOGIES
+ */
+
+function languagesHover () {
+    python.style.transition = '0.2s';
+    python.style.borderRadius = '10px';
+
+    
+    elixir.style.transition = '0.2s';
+    elixir.style.borderRadius = '10px';
+
+    bash.style.transition = '0.2s';
+    bash.style.borderRadius = '10px';
+
+    ruby.style.transition = '0.2s';
+    ruby.style.borderRadius = '10px';
+
+    typeScript.style.transition = '0.2s';
+    typeScript.style.borderRadius = '10px';
+
+    go.style.transition = '0.2s';
+    go.style.borderRadius = '10px';
+
+    scala.style.transition = '0.2s';
+    scala.style.borderRadius = '10px';
+
+    erlang.style.transition = '0.2s';
+    erlang.style.borderRadius = '10px';
+
+    kotlin.style.transition = '0.2s';
+    kotlin.style.borderRadius = '10px';
+
+    c_sharp.style.transition = '0.2s';
+    c_sharp.style.borderRadius = '10px';
+
+    java.style.transition = '0.2s';
+    java.style.borderRadius = '10px';
+
+    php.style.transition = '0.2s';
+    php.style.borderRadius = '10px';
+
+    webAssembly.style.transition = '0.2s';
+    webAssembly.style.borderRadius = '10px';
+
+    javaScript.style.transition = '0.2s';
+    javaScript.style.borderRadius = '10px';
+
+    c_plus.style.transition = '0.2s';
+    c_plus.style.borderRadius = '10px';
+
+    c.style.transition = '0.2s';
+    c.style.borderRadius = '10px';
+
+    swift.style.transition = '0.2s';
+    swift.style.borderRadius = '10px';
+
+    dart.style.transition = '0.2s';
+    dart.style.borderRadius = '10px';
+
+    r.style.transition = '0.2s';
+    r.style.borderRadius = '10px';
+
+    assembly.style.transition = '0.2s';
+    assembly.style.borderRadius = '10px';
+
+    rust.style.transition = '0.2s';
+    rust.style.borderRadius = '10px';
+
+    clojure.style.transition = '0.2s';
+    clojure.style.borderRadius = '10px';
+
+    sql.style.transition = '0.2s';
+    sql.style.borderRadius = '10px';
+
+    htmlCss.style.transition = '0.2s';
+    htmlCss.style.borderRadius = '10px';
+
+
+    (function () {
+        // FRAMEWORKS
+        react.style.opacity = 0.4;
+        vue.style.opacity = 0.4;
+        spring.style.opacity = 0.4;
+        flask.style.opacity = 0.4;
+        angular.style.opacity = 0.4;
+        jquery.style.opacity = 0.4;
+        express.style.opacity = 0.4;
+        asp.style.opacity = 0.4;
+        laravel.style.opacity = 0.4;
+        rubyOnRails.style.opacity = 0.4;
+        drumpal.style.opacity = 0.4;
+        django.style.opacity = 0.4;
+
+        // DB Opacity
+        elastic.style.opacity = 0.4;
+        postgre.style.opacity = 0.4;
+        maria.style.opacity = 0.4;
+        sqLite.style.opacity = 0.4;
+        dynamo.style.opacity = 0.4;
+        redis.style.opacity = 0.4;
+        firebase.style.opacity = 0.4;
+        mongo.style.opacity = 0.4;
+        msSQL.style.opacity = 0.4;
+        node.style.opacity = 0.4;
+        mySQL.style.opacity = 0.4;
+    }());
 
 }
+
+function languagesRevert () {
+    python.style.borderRadius = '0';
+    python.style.fontWeight = 'normal';
+
+    
+    elixir.style.borderRadius = '0';
+    elixir.style.fontWeight = 'normal';
+
+    bash.style.borderRadius = '0';
+    bash.style.fontWeight = 'normal';
+
+    ruby.style.borderRadius = '0';
+    ruby.style.fontWeight = 'normal';
+
+    typeScript.style.borderRadius = '0';
+    typeScript.style.fontWeight = 'normal';
+
+    go.style.borderRadius = '0';
+    go.style.fontWeight = 'normal';
+
+    scala.style.borderRadius = '0';
+    scala.style.fontWeight = 'normal';
+
+    erlang.style.borderRadius = '0';
+    erlang.style.fontWeight = 'normal';
+
+    kotlin.style.borderRadius = '0';
+    kotlin.style.fontWeight = 'normal';
+
+    c_sharp.style.borderRadius = '0';
+    c_sharp.style.fontWeight = 'normal';
+
+    java.style.borderRadius = '0';
+    java.style.fontWeight = 'normal';
+
+    php.style.borderRadius = '0';
+    php.style.fontWeight = 'normal';
+
+    webAssembly.style.borderRadius = '0';
+    webAssembly.style.fontWeight = 'normal';
+
+    javaScript.style.borderRadius = '0';
+    javaScript.style.fontWeight = 'normal';
+
+    c_plus.style.borderRadius = '0';
+    c_plus.style.fontWeight = 'normal';
+
+    c.style.borderRadius = '0';
+    c.style.fontWeight = 'normal';
+
+    swift.style.borderRadius = '0';
+    swift.style.fontWeight = 'normal';
+
+    dart.style.borderRadius = '0';
+    dart.style.fontWeight = 'normal';
+
+    r.style.borderRadius = '0';
+    r.style.fontWeight = 'normal';
+
+    assembly.style.borderRadius = '0';
+    assembly.style.fontWeight = 'normal';
+
+    rust.style.borderRadius = '0';
+    rust.style.fontWeight = 'normal';
+
+    clojure.style.borderRadius = '0';
+    clojure.style.fontWeight = 'normal';
+
+    sql.style.borderRadius = '0';
+    sql.style.fontWeight = 'normal';
+
+    htmlCss.style.borderRadius = '0';
+    htmlCss.style.fontWeight = 'normal';
+
+    (function() {
+        // FRAMEWORKS
+        react.style.opacity = 1;
+        vue.style.opacity = 1;
+        spring.style.opacity = 1;
+        flask.style.opacity = 1;
+        angular.style.opacity = 1;
+        jquery.style.opacity = 1;
+        express.style.opacity = 1;
+        asp.style.opacity = 1;
+        laravel.style.opacity = 1;
+        rubyOnRails.style.opacity = 1;
+        drumpal.style.opacity = 1;
+        django.style.opacity = 1;
+
+        // DB
+        elastic.style.opacity = 1;
+        postgre.style.opacity = 1;
+        maria.style.opacity = 1;
+        sqLite.style.opacity = 1;
+        dynamo.style.opacity = 1;
+        redis.style.opacity = 1;
+        firebase.style.opacity = 1;
+        mongo.style.opacity = 1;
+        msSQL.style.opacity = 1;
+        node.style.opacity = 1;
+        mySQL.style.opacity = 1;
+    }());
+
+}
+
+document.getElementById('lang-btn').addEventListener("mouseenter", languagesHover);
+document.getElementById('lang-btn').addEventListener("mouseleave", languagesRevert);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
+ * WHEN YOU HOVER OVER DATABASES BUTTON STYLE DATABASES AND DIM OTHER TECHNOLOGIES
+ */
+
+function dbHover () {
+    elastic.style.transition = '0.2s';
+    elastic.style.borderRadius = '10px';
+
+    
+    postgre.style.transition = '0.2s';
+    postgre.style.borderRadius = '10px';
+
+    maria.style.transition = '0.2s';
+    maria.style.borderRadius = '10px';
+
+    sqLite.style.transition = '0.2s';
+    sqLite.style.borderRadius = '10px';
+
+    dynamo.style.transition = '0.2s';
+    dynamo.style.borderRadius = '10px';
+
+    redis.style.transition = '0.2s';
+    redis.style.borderRadius = '10px';
+
+    firebase.style.transition = '0.2s';
+    firebase.style.borderRadius = '10px';
+
+    mongo.style.transition = '0.2s';
+    mongo.style.borderRadius = '10px';
+
+    msSQL.style.transition = '0.2s';
+    msSQL.style.borderRadius = '10px';
+
+    node.style.transition = '0.2s';
+    node.style.borderRadius = '10px';
+
+    mySQL.style.transition = '0.2s';
+    mySQL.style.borderRadius = '10px';
+
+    (function () {
+        python.style.opacity = 0.4;
+        elixir.style.opacity = 0.4;
+        bash.style.opacity = 0.4;
+        ruby.style.opacity = 0.4;
+        typeScript.style.opacity = 0.4;
+        go.style.opacity = 0.4;
+        scala.style.opacity = 0.4;
+        erlang.style.opacity = 0.4;
+        kotlin.style.opacity = 0.4;
+        c_sharp.style.opacity = 0.4;
+        java.style.opacity = 0.4;
+        php.style.opacity = 0.4;
+        webAssembly.style.opacity = 0.4;
+        javaScript.style.opacity = 0.4;
+        c_plus.style.opacity = 0.4;
+        c.style.opacity = 0.4;
+        swift.style.opacity = 0.4;
+        dart.style.opacity = 0.4;
+        r.style.opacity = 0.4;
+        assembly.style.opacity = 0.4;
+        rust.style.opacity = 0.4;
+        clojure.style.opacity = 0.4;
+        sql.style.opacity = 0.4;
+        htmlCss.style.opacity = 0.4;
+
+        // FRAMEWORKS
+        react.style.opacity = 0.4;
+        vue.style.opacity = 0.4;
+        spring.style.opacity = 0.4;
+        flask.style.opacity = 0.4;
+        angular.style.opacity = 0.4;
+        jquery.style.opacity = 0.4;
+        express.style.opacity = 0.4;
+        asp.style.opacity = 0.4;
+        laravel.style.opacity = 0.4;
+        rubyOnRails.style.opacity = 0.4;
+        drumpal.style.opacity = 0.4;
+        django.style.opacity = 0.4;
+    }());
+
+};
+
+function dbRevert () {
+    elastic.style.borderRadius = '0';
+    elastic.style.fontWeight = 'normal';
+
+    
+    postgre.style.borderRadius = '0';
+    postgre.style.fontWeight = 'normal';
+
+    maria.style.borderRadius = '0';
+    maria.style.fontWeight = 'normal';
+
+    sqLite.style.borderRadius = '0';
+    sqLite.style.fontWeight = 'normal';
+
+    dynamo.style.borderRadius = '0';
+    dynamo.style.fontWeight = 'normal';
+
+    redis.style.borderRadius = '0';
+    redis.style.fontWeight = 'normal';
+
+    firebase.style.borderRadius = '0';
+    firebase.style.fontWeight = 'normal';
+
+    mongo.style.borderRadius = '0';
+    mongo.style.fontWeight = 'normal';
+
+    msSQL.style.borderRadius = '0';
+    msSQL.style.fontWeight = 'normal';
+
+    node.style.borderRadius = '0';
+    node.style.fontWeight = 'normal';
+
+    mySQL.style.borderRadius = '0';
+    mySQL.style.fontWeight = 'normal';
+
+    (function() {
+        // FRAMEWORKS
+        react.style.opacity = 1;
+        vue.style.opacity = 1;
+        spring.style.opacity = 1;
+        flask.style.opacity = 1;
+        angular.style.opacity = 1;
+        jquery.style.opacity = 1;
+        express.style.opacity = 1;
+        asp.style.opacity = 1;
+        laravel.style.opacity = 1;
+        rubyOnRails.style.opacity = 1;
+        drumpal.style.opacity = 1;
+        django.style.opacity = 1;
+
+        python.style.opacity = 1
+        python.style.opacity = 1
+        elixir.style.opacity = 1
+        bash.style.opacity = 1
+        ruby.style.opacity = 1
+        typeScript.style.opacity = 1
+        go.style.opacity = 1
+        scala.style.opacity = 1
+        erlang.style.opacity = 1
+        kotlin.style.opacity = 1
+        c_sharp.style.opacity = 1
+        java.style.opacity = 1
+        php.style.opacity = 1
+        webAssembly.style.opacity = 1
+        javaScript.style.opacity = 1
+        c_plus.style.opacity = 1
+        c.style.opacity = 1
+        swift.style.opacity = 1
+        dart.style.opacity = 1
+        r.style.opacity = 1
+        assembly.style.opacity = 1
+        rust.style.opacity = 1
+        clojure.style.opacity = 1
+        sql.style.opacity = 1
+        htmlCss.style.opacity = 1
+
+    }());
+
+};
+
+ document.getElementById('db-btn').addEventListener("mouseenter", dbHover);
+ document.getElementById('db-btn').addEventListener("mouseleave", dbRevert);
